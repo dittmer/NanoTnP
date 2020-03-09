56674c88e6fa0d67ec2c852ddef4c78249eacf9b
siewyanhoh <siew.yan.hoh@cern.ch>
2020-03-09 16:41:49 +0100

    remove unused files

--- run.py
+++ run.py
@@ -18,7 +18,7 @@ if not os.path.exists(output):
 
 tcompile = time.time()
 os.system("make")
-print("--- compilation %.3f seconds (%.3f minutes) ---" % ( (time.time() - tcompile) , (time.time() - tcompile)/60. ) )
+print("--- compilation took : %.3f seconds (%.3f minutes) ---" % ( (time.time() - tcompile) , (time.time() - tcompile)/60. ) )
 
 samplelists=[]
 lumi=""
@@ -33,16 +33,36 @@ if datasets == '2017':
     # single electrons
     samplelists.append("%s/samplelists/latinov5_17/SingleElectron_Run2017.txt" %cwd)
 
-elif datasets == '2017':
+elif datasets == '2018':
+    lumi = "59.74"
+    # DY LO
     samplelists.append("%s/samplelists/latinov5_17/DYJetsToLL_M-50-LO.txt" %cwd)
+    # alt DY NLO
+    samplelists.append("%s/samplelists/latinov5_17/DYJetsToLL_M-50_ext1.txt" %cwd)
+    # single electrons
+    samplelists.append("%s/samplelists/latinov5_17/SingleElectron_Run2017.txt" %cwd)
+
+else:
+    lumi = "35.867"
+    # DY LO
+    samplelists.append("%s/samplelists/latinov5_17/DYJetsToLL_M-50-LO.txt" %cwd)
+    # alt DY NLO
+    samplelists.append("%s/samplelists/latinov5_17/DYJetsToLL_M-50_ext1.txt" %cwd)
+    # single electrons
+    samplelists.append("%s/samplelists/latinov5_17/SingleElectron_Run2017.txt" %cwd)
+
 
 print("year : %s" %(datasets))
 print("lumi : %s 1/fb" %(lumi))
 
 if len(samplelists)!=3: print("ERROR: len(samplelists)!=3"); sys.exit(0);
 
+trun = time.time();
 for iproc in samplelists:
     sample = iproc.split('/')[-1].split('.txt')[0]
     cmd="./skim"; cmd+=" %s %s/%s.root %s" %(iproc,output,sample,lumi)
+    tproc = time.time()
     print(cmd)
     os.system(cmd)
+    print("--- running on %s took : %.3f seconds (%.3f minutes) ---" % ( sample , (time.time() - tproc) , (time.time() - tproc)/60. ) )
+print("--- Total run time : %.3f seconds (%.3f minutes) ---" % ( (time.time() - trun) , (time.time() - trun)/60. ) )
