207a37841f7808b535f4e9acdb03b8ba9fc89874
siewyanhoh <siew.yan.hoh@cern.ch>
2020-03-17 12:02:17 +0100

    added EGM fitter

---
+++ egm_tnp_analysis/settings.py
@@ -0,0 +1,105 @@
+#############################################################
+########## General settings
+#############################################################
+import os
+# flag to be Tested
+#cutpass80 = '(( abs(probe_sc_eta) < 0.8 && probe_Ele_nonTrigMVA > %f ) ||  ( abs(probe_sc_eta) > 0.8 && abs(probe_sc_eta) < 1.479&& probe_Ele_nonTrigMVA > %f ) || ( abs(probe_sc_eta) > 1.479 && probe_Ele_nonTrigMVA > %f ) )' % (0.988153,0.967910,0.841729)
+#cutpass90 = '(( abs(probe_sc_eta) < 0.8 && probe_Ele_nonTrigMVA > %f ) ||  ( abs(probe_sc_eta) > 0.8 && abs(probe_sc_eta) < 1.479&& probe_Ele_nonTrigMVA > %f ) || ( abs(probe_sc_eta) > 1.479 && probe_Ele_nonTrigMVA > %f ) )' % (0.972153,0.922126,0.610764)
+
+# flag to be Tested
+flag = {
+#    'passingCutBasedIDFall17V2' : '(passingCutBasedIDFall17V2 == 1)',
+    'passingmvaTTH'             : '(passingMVAtth == 1)',
+}
+
+#baseOutDir = 'results/runB/%s/' % (flag.keys()[0])
+baseOutDir = '%s/results/Legacy2016/tnpEleID/runAll' %os.getcwd()
+
+#############################################################
+########## samples definition  [can be nD bining]
+#############################################################
+#tnpTreeDir = 'GsfElectronToEleID'
+tnpTreeDir = 'Events'
+weightName = 'weight'
+
+### MANDATORY nEvts in data = -1 // mcTruth will require mc Matching
+## some sample based cuts... general cuts defined here after
+cutAltSel = 'tag_Ele_pt > 30'
+#&& tag_Ele_nonTrigMVA > 0.90'
+#cutData   = 'run >= 273726'
+cutData    = None
+
+#* samplesDef: these are the main info
+#  - data: data ntuple
+#  - mcNom: nominal MC sample
+#  - mcAlt: MC for generator syst
+#  - tagSel: usually same as nominal MC + different base cuts: check the tag selection syst
+
+samplesDef = {
+    'data'     : { 'name' : 'SingleElectron' , 'mcTruth' : False, 'nEvts': -1, 'cut' : cutData,
+                   'path' : '../skim/2016/SingleElectron_Skim.root' },
+    'mcNom'    : { 'name' : 'DYJetsToLL_M-50-LONom' , 'mcTruth' : False , 'nEvts': -1, 'cut' : None,
+                   'path' : '../skim/2016/DYJetsToLL_M-50-LO_ext2_Skim.root' },
+    'mcAlt'    : None,
+    'tagSel'   : { 'name' : 'DYJetsToLL_M-50-LOAltSel' , 'mcTruth' : False , 'nEvts' : -1, 'cut' : cutAltSel,
+                   'path' : '../skim/2016/DYJetsToLL_M-50-LO_ext2_Skim.root' },
+}
+
+### lumi in /fb
+lumi = 35.867
+
+#############################################################
+########## bining definition  [can be nD bining]
+#############################################################
+biningDef = [
+   { 'var' : 'probe_Ele_eta' , 'type': 'float', 'bins': [-2.5,-2.0,-1.566,-1.442, -1.0, 0.0, 1.0, 1.442, 1.566, 2.0, 2.5] },
+   { 'var' : 'probe_Ele_pt' , 'type': 'float', 'bins': [10,20.0,30,40,50,200] },
+]
+
+#############################################################
+########## Cuts definition for all samples
+#############################################################
+### cut
+cutBase   = 'tag_Ele_pt > 30 && abs(tag_Ele_eta) < 2.1'
+
+# can add addtionnal cuts for some bins (first check bin number using tnpEGM --checkBins)
+additionalCuts = { 
+#    0 : 'tag_Ele_trigMVA > 0.92 && sqrt( 2*event_met_pfmet*tag_Ele_pt*(1-cos(event_met_pfphi-tag_Ele_phi))) < 45',
+#    1 : 'tag_Ele_trigMVA > 0.92 && sqrt( 2*event_met_pfmet*tag_Ele_pt*(1-cos(event_met_pfphi-tag_Ele_phi))) < 45',
+#    2 : 'tag_Ele_trigMVA > 0.92 && sqrt( 2*event_met_pfmet*tag_Ele_pt*(1-cos(event_met_pfphi-tag_Ele_phi))) < 45',
+#    3 : 'tag_Ele_trigMVA > 0.92 && sqrt( 2*event_met_pfmet*tag_Ele_pt*(1-cos(event_met_pfphi-tag_Ele_phi))) < 45',
+#    4 : 'tag_Ele_trigMVA > 0.92 && sqrt( 2*event_met_pfmet*tag_Ele_pt*(1-cos(event_met_pfphi-tag_Ele_phi))) < 45',
+#    5 : 'tag_Ele_trigMVA > 0.92 && sqrt( 2*event_met_pfmet*tag_Ele_pt*(1-cos(event_met_pfphi-tag_Ele_phi))) < 45',
+#    6 : 'tag_Ele_trigMVA > 0.92 && sqrt( 2*event_met_pfmet*tag_Ele_pt*(1-cos(event_met_pfphi-tag_Ele_phi))) < 45',
+#    7 : 'tag_Ele_trigMVA > 0.92 && sqrt( 2*event_met_pfmet*tag_Ele_pt*(1-cos(event_met_pfphi-tag_Ele_phi))) < 45',
+#    8 : 'tag_Ele_trigMVA > 0.92 && sqrt( 2*event_met_pfmet*tag_Ele_pt*(1-cos(event_met_pfphi-tag_Ele_phi))) < 45',
+#    9 : 'tag_Ele_trigMVA > 0.92 && sqrt( 2*event_met_pfmet*tag_Ele_pt*(1-cos(event_met_pfphi-tag_Ele_phi))) < 45'
+}
+
+#### or remove any additional cut (default)
+#additionalCuts = None
+
+#############################################################
+########## fitting params to tune fit by hand if necessary
+#############################################################
+tnpParNomFit = [
+    "meanP[-0.0,-5.0,5.0]","sigmaP[0.5,0.1,5.0]",
+    "meanF[-0.0,-5.0,5.0]","sigmaF[0.5,0.1,5.0]",
+    "acmsP[60.,50.,80.]","betaP[0.05,0.01,0.08]","gammaP[0.1, 0, 1]","peakP[90.0]",
+    "acmsF[60.,50.,80.]","betaF[0.05,0.01,0.08]","gammaF[0.1, 0, 1]","peakF[90.0]",
+    ]
+
+tnpParAltSigFit = [
+    "meanP[-0.0,-5.0,5.0]","sigmaP[1,0.7,6.0]","alphaP[2.0,1.2,3.5]" ,'nP[3,0.05,5]',"sigmaP_2[1.5,0.5,6.0]",
+    "meanF[-0.0,-5.0,5.0]","sigmaF[2,0.7,15.0]","alphaF[2.0,1.2,3.5]",'nF[3,0.05,5]',"sigmaF_2[2.0,0.5,6.0]",
+    "acmsP[60.,50.,75.]","betaP[0.04,0.01,0.06]","gammaP[0.1, 0.005, 1]","peakP[90.0]",
+    "acmsF[60.,50.,75.]","betaF[0.04,0.01,0.06]","gammaF[0.1, 0.005, 1]","peakF[90.0]",
+    ]
+     
+tnpParAltBkgFit = [
+    "meanP[-0.0,-5.0,5.0]","sigmaP[0.5,0.1,5.0]",
+    "meanF[-0.0,-5.0,5.0]","sigmaF[0.5,0.1,5.0]",
+    "alphaP[0.,-5.,5.]",
+    "alphaF[0.,-5.,5.]",
+    ]
+        
