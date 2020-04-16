bd9e0971c1e67549140b4afd1ac743aa826dfbe9
SiewYan <siew.yan.hoh@cern.ch>
2020-04-16 05:25:59 +0200

    update validation plot

---
+++ lxplus_setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "source /cvmfs/sft.cern.ch/lcg/views/LCG_95apython3/x86_64-centos7-gcc7-opt/setup.sh"
+source /cvmfs/sft.cern.ch/lcg/views/LCG_95apython3/x86_64-centos7-gcc7-opt/setup.sh
