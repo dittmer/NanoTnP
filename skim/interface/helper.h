831633a3a6f7e4b54beaf9d11134b57c53d92fee
SiewYan <siew.yan.hoh@cern.ch>
2020-05-02 07:33:36 +0200

    first commit skim

---
+++ skim/interface/helper.h
@@ -0,0 +1,165 @@
+#ifndef HELPER_H
+#define HELPER_H
+#include "ROOT/RDataFrame.hxx"
+#include "ROOT/RDFHelpers.hxx"
+#include "ROOT/RVec.hxx"
+
+#include "Math/Vector4D.h"
+#include "TStopwatch.h"
+
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <map>
+
+#include "utility" // std::pair
+#include <algorithm> // for std::find
+#include <iterator> // for std::begin, std::end
+
+#include "TRandom3.h"
+#include "TLorentzVector.h"
+
+namespace Helper {
+
+  /*
+   * sort Gen matched candidates in ascending order
+   */
+  struct dRSorter {
+    bool operator() (std::pair<std::pair<int,int>,float> i , std::pair<std::pair<int,int>,float> j) { return ( (i.second) < (j.second) ); }
+  };
+
+  template <typename T>
+    std::vector<int> IndexBydeltaR(T v){
+    dRSorter comparator;
+    std::vector<int> indecies;
+    std::sort (v.begin() , v.end() , comparator);
+    for (auto it = v.begin() ; it != v.end() ; ++it){
+      indecies.push_back((*it).first.second);
+    }
+    return indecies;
+  }
+  
+  /*
+   * bit decoder
+   */
+  template <typename T>
+    int bitdecoder( T decimal , T kbit){
+    // shift to specific bit to the left, and query on/off state
+    return (decimal >> kbit & 1);
+  }
+  
+  /*
+   * Compute the difference in the azimuth coordinate taking the boundary conditions at 2*pi into account.
+   */
+  template <typename T>
+    float DeltaPhi(T v1, T v2, const T c = M_PI)
+    {
+      auto r = std::fmod(v2 - v1, 2.0 * c);
+      if (r < -c) {
+	r += 2.0 * c;
+      }
+      else if (r > c) {
+	r -= 2.0 * c;
+      }
+      return r;
+    }
+
+  template<typename T>
+    TLorentzVector VectorMaker(T pt, T eta, T phi, T m){
+    TLorentzVector out;
+    out.SetPtEtaPhiM(pt,eta,phi,m);
+    return out;
+  }
+
+  /*
+   * Json reader (author: the overlord Afiq @DESY)
+   */
+  template <typename T>
+    std::map<int, std::vector<std::pair<int, int> > > parseJSONAsMap(const T jsonFile) {
+    std::map<int, std::vector<std::pair<int, int> > > m_json;
+    
+    std::ifstream json(jsonFile);
+    std::string line;
+
+    while (getline(json, line)) {
+      
+      const unsigned qCount = std::count(line.begin(), line.end(), '"');
+      if (qCount < 2 or qCount % 2 != 0) continue; // skip this line, weird that we don't have even quote count
+      
+      unsigned q1 = line.find('"');
+      while (q1 < line.length()) {
+
+	unsigned q2 = line.find('"', q1 + 1);
+	
+	unsigned bo1 = line.find('[', q1 + 1);
+	if (line.find('[', bo1 + 1) == bo1 + 1)
+	  ++bo1;
+  
+	unsigned bc1 = line.find(']', bo1 + 1);
+	if (bo1 > q1 and bo1 > q2) {
+	  const int nRun = std::stoi( line.substr(q1 + 1, q2 - q1 - 1) );
+	  std::vector<std::pair<int, int> > vp_lumi;
+	  while (true) {
+	    if (bc1 > bo1) {
+	      const std::string sLumi = line.substr(bo1 + 1, bc1 - bo1 - 1);
+	      const unsigned comma = sLumi.find(',');
+	      
+	      vp_lumi.push_back( std::make_pair(std::stoi(sLumi.substr(0, comma)),
+						std::stoi(sLumi.substr(comma + 1, sLumi.length() - comma))) );
+	    }
+	    if (line.find(']', bc1 + 1) == bc1 + 1) break; 
+	    bc1 = line.find(']', bc1 + 1);
+	    bo1 = line.find('[', bo1 + 1);
+	  } 
+	  m_json.insert( std::make_pair(nRun, vp_lumi) );
+	}	
+	q1 = line.find('"', q2 + 1);
+      }
+    }    
+    return m_json;
+  }
+ 
+  /*
+   * json selector (author: the overlord Afiq @DESY)
+   */
+  template <typename T>
+    bool isRunLumiInJSON(const std::map<T, std::vector<std::pair<T, T> > > m_json, T nRun, T nLumi) {
+    
+    if (m_json.empty())
+      return true;
+    
+    for (auto &p_json : m_json) {
+      if (nRun == p_json.first) {
+	for (auto &p_lumi : p_json.second) {
+	  if (nLumi >= p_lumi.first and nLumi <= p_lumi.second)
+	    return true;
+	}
+      }
+    }
+    return false;
+  }
+
+  /**  
+  struct config_t {
+    // basic
+    bool isMC=false;
+    bool passHLT=false;
+    // jet cleaning
+    float jetclean_dR=0.3;
+    //trigger
+    std::string name = "NULL";
+    std::string bit = "NULL";
+    // tag and pair flag
+    bool resolveAmbiguity=false;
+    // cert
+    std::string jsonFile;
+    //std::map<int, std::vector<std::pair<int, int> > > jsonFile;
+  };
+  **/
+} //helper
+
+#endif
---
+++ skim/interface/tnpPairs_sequence.h
@@ -0,0 +1,94 @@
+#ifndef TNPPAIRS_SEQUENCE_H
+#define TNPPAIRS_SEQUENCE_H
+#include "config.h"
+#include "helper.h"
+
+/* tnpEleIDs 
+ * note: its reco object pairing, no gen-selection
+*/
+template <typename T>
+auto tnpPairingEleIDs(T &df){
+  using namespace ROOT::VecOps;
+  //####### Lambda function
+  auto tnpPairing = [](
+		       RVec<int>& tagele ,
+		       RVec<int>& probeele,
+		       RVec<float>& pt ,
+		       RVec<float>& eta ,
+		       RVec<float>& phi ,
+		       RVec<float>& m ,
+		       RVec<int>& eleIdx
+		       )
+    {
+      auto comb = Combinations(tagele,probeele);
+      const auto paircomb = comb[0].size();
+      std::vector<std::pair<std::pair<int,int>,float>> pair_candidate; // store idx and mass
+      // step 1 , make pair 
+      for(size_t i = 0 ; i < paircomb ; i++){
+	const auto tagIdx = comb[0][i];
+	const auto probeIdx = comb[1][i];
+	if (tagIdx==probeIdx) continue;
+	//passing tag and probe flag gaurantee were looking at electron in Lepton collection
+	if (!tagele[tagIdx]) continue;
+	if (!probeele[probeIdx]) continue;
+	//recover Idx for Electron collection
+	auto tag_eleIdx= eleIdx[tagIdx]; auto probe_eleIdx= eleIdx[probeIdx];
+	TLorentzVector tot = Helper::VectorMaker( pt[tagIdx] , eta[tagIdx] , phi[tagIdx] , m[tag_eleIdx] );
+	TLorentzVector probe = Helper::VectorMaker( pt[probeIdx] , eta[probeIdx] , phi[probeIdx] , m[probe_eleIdx] );
+	tot+=probe;
+	if ( tot.M() > 50 && tot.M() < 130 ) pair_candidate.push_back(std::make_pair(std::make_pair(tagIdx,probeIdx),tot.M()));
+      }
+      // step 2 , select pair closest to the Z mass pole, 91.18 GeV
+      auto nTnP = pair_candidate.size();
+      float zmassMin=999.; int pairIdx = -1, tIdx = -1, pIdx = -1; int randomness = -1.; std::pair<int,int> tagProbePair;
+      if (nTnP==0){
+	return std::vector<int>({tIdx , pIdx , static_cast<int>(nTnP) , pairIdx , randomness });
+      }
+      else if (nTnP==1){
+	tagProbePair=pair_candidate[0].first;
+      }
+      else {
+	for (size_t ipair = 0 ; ipair < nTnP ; ipair++){
+	  auto zmass = pair_candidate[ipair].second;
+	  if ( fabs(zmass-91.18) < zmassMin ){
+	    zmassMin = fabs(zmass-91.18);
+	    pairIdx = ipair;
+	  }//
+	} // end of for loop
+	tagProbePair=pair_candidate[pairIdx].first;
+      } // end of if
+      
+      // step 3 , randomize tag selection if two electron statisfies tag selection
+      // if first is tag and second does not satisfy tag
+      if ( tagele[tagProbePair.first] && !tagele[tagProbePair.second] ){
+	tIdx = tagProbePair.first; pIdx = tagProbePair.second;
+      }
+      // both electrons satisfy tag, randomize selection
+      else if ( tagele[tagProbePair.first] && tagele[tagProbePair.second] ){
+	// throw random number
+	TRandom3 generator = TRandom3(0);
+	randomness = static_cast<int>(generator.Uniform(1.,5.)) % 2 ;
+	if ( randomness == 0 ){ //even
+	  tIdx = tagProbePair.first; pIdx = tagProbePair.second;
+	}
+	else{
+	  tIdx = tagProbePair.second; pIdx = tagProbePair.first;
+	}
+      } // end of if
+      
+      return std::vector<int>({ tIdx , pIdx , static_cast<int>(nTnP) , pairIdx , randomness });
+      
+    };
+  //####### Lambda function
+  return df
+    .Define("tnpPairing", tnpPairing, { "tagEle" , "probeEle" , "Lepton_pt" , "Lepton_eta" , "Lepton_phi" , "Electron_mass" , "Lepton_electronIdx" } )
+    .Define("tag_Idx", "tnpPairing[0]")
+    .Define("probe_Idx","tnpPairing[1]")
+    .Define("nTnP","tnpPairing[2]")
+    .Define("ipair","tnpPairing[3]")
+    .Define("randomness","tnpPairing[4]")
+    .Define("tnpPairingEleID","tag_Idx!=-1 && probe_Idx!=-1")
+    ;
+}
+
+#endif
