e49526304bf41965a957647a8e0c2bf655ef73ca
SiewYan <siew.yan.hoh@cern.ch>
2020-06-02 05:37:36 +0200

    update validation

---
+++ validation/kinematics/baseline/helper.py
@@ -0,0 +1,123 @@
+from array import array
+import math
+from ROOT import gPad, TCanvas, TH2D, TH1D, TFile, THStack, TLegend, gSystem, TLatex, TH1, gStyle, TGaxis
+import os, sys
+
+#TGaxis.SetMaxDigits(2)
+TH1.SetDefaultSumw2()
+gStyle.SetPaintTextFormat(".5f")
+
+def setBotStyle(h, r=4, fixRange=True):
+    h.GetXaxis().SetLabelSize(h.GetXaxis().GetLabelSize()*(r-1));
+    h.GetXaxis().SetLabelOffset(h.GetXaxis().GetLabelOffset()*(r-1));
+    h.GetXaxis().SetTitleSize(h.GetXaxis().GetTitleSize()*(r-1));
+    h.GetYaxis().SetLabelSize(h.GetYaxis().GetLabelSize()*(r-1));
+    h.GetYaxis().SetNdivisions(505);
+    h.GetYaxis().SetTitleSize(h.GetYaxis().GetTitleSize()*(r-1));
+    h.GetYaxis().SetTitleOffset(h.GetYaxis().GetTitleOffset()/(r-1));
+    if fixRange:
+        h.GetYaxis().SetRangeUser(0., 2.)
+        for i in range(1, h.GetNbinsX()+1):
+            if h.GetBinContent(i)<1.e-6:
+                h.SetBinContent(i, -1.e-6)
+pass
+
+def drawCMS(lumi, text, onTop=False):
+    latex = TLatex()
+    latex.SetNDC()
+    latex.SetTextSize(0.04)
+    latex.SetTextColor(1)
+    latex.SetTextFont(42)
+    latex.SetTextAlign(33)
+    if (type(lumi) is float or type(lumi) is int) and float(lumi) > 0: latex.DrawLatex(0.95, 0.985, "%.1f fb^{-1}  (13 TeV)" % (float(lumi)/1000.))
+    elif type(lumi) is str: latex.DrawLatex(0.95, 0.985, "%s fb^{-1}  (13 TeV)" % lumi)
+    if not onTop: latex.SetTextAlign(11)
+    latex.SetTextFont(62)
+    latex.SetTextSize(0.05 if len(text)>0 else 0.06)
+    if not onTop: latex.DrawLatex(0.15, 0.87 if len(text)>0 else 0.84, "CMS")
+    else: latex.DrawLatex(0.20, 0.99, "CMS")
+    latex.SetTextSize(0.04)
+    latex.SetTextFont(52)
+    if not onTop: latex.DrawLatex(0.15, 0.83, text)
+    else: latex.DrawLatex(0.40, 0.98, text)
+pass
+
+def setHistStyle(hist, r=1.1):
+    hist.GetXaxis().SetTitleSize(hist.GetXaxis().GetTitleSize()*r*r)
+    hist.GetYaxis().SetTitleSize(hist.GetYaxis().GetTitleSize()*r*r)
+    hist.GetXaxis().SetLabelSize(hist.GetXaxis().GetLabelSize()*r)
+    hist.GetYaxis().SetLabelSize(hist.GetYaxis().GetLabelSize()*r)
+    hist.GetXaxis().SetLabelOffset(hist.GetXaxis().GetLabelOffset()*r*r*r*r)
+    hist.GetXaxis().SetTitleOffset(hist.GetXaxis().GetTitleOffset()*r)
+    hist.GetYaxis().SetTitleOffset(hist.GetYaxis().GetTitleOffset())
+    if hist.GetXaxis().GetTitle().find("GeV") != -1: # and not hist.GetXaxis().IsVariableBinSize()
+        div = (hist.GetXaxis().GetXmax() - hist.GetXaxis().GetXmin()) / hist.GetXaxis().GetNbins()
+        hist.GetYaxis().SetTitle("Events / %.1f GeV" % div)
+pass
+
+def drawRatio(data, bkg):
+    errData = array('d', [1.0])
+    errBkg = array('d', [1.0])
+    intData = data.IntegralAndError(1, data.GetNbinsX(), errData)
+    intBkg = bkg.IntegralAndError(1, bkg.GetNbinsX(), errBkg)
+    ratio = intData / intBkg if intBkg!=0 else 0.
+    error = math.hypot(errData[0]*ratio/intData,  errBkg[0]*ratio/intBkg) if intData>0 and intBkg>0 else 0
+    latex = TLatex()
+    latex.SetNDC()
+    latex.SetTextColor(1)
+    latex.SetTextFont(62)
+    latex.SetTextSize(0.08)
+    #latex.DrawLatex(0.25, 0.85, "Data/Bkg = %.3f #pm %.3f" % (ratio, error))
+    latex.DrawLatex(0.15, 0.85, "Data/Bkg = %.3f #pm %.3f" % (ratio, error))
+    print ("  Ratio:\t%.3f +- %.3f" % (ratio, error))
+    return [ratio, error]
+pass
+
+def drawKolmogorov(data, bkg):
+    latex = TLatex()
+    latex.SetNDC()
+    latex.SetTextColor(1)
+    latex.SetTextFont(62)
+    latex.SetTextSize(0.08)
+    #latex.DrawLatex(0.45, 0.85, "#chi^{2}/ndf = %.2f,   K-S = %.3f" % (data.Chi2Test(bkg, "CHI2/NDF"), data.KolmogorovTest(bkg)))
+    latex.DrawLatex(0.45, 0.85, "#chi^{2}/ndf = %.2f" % ( data.Chi2Test(bkg, "CHI2/NDF") ) )
+    
+pass
+
+def drawRelativeYield(data,bkg):
+    latex = TLatex()
+    latex.SetNDC()
+    latex.SetTextColor(1)
+    latex.SetTextFont(62)
+    latex.SetTextSize(0.08)
+    latex.DrawLatex(0.75, 0.85, "rel. Yield= %.3f" % ((data.Integral()/bkg.Integral())*100) )
+pass
+
+def setTopPad(TopPad, r=4):
+    TopPad.SetPad("TopPad", "", 0., 1./r, 1.0, 1.0, 0, -1, 0)
+    TopPad.SetTopMargin(0.24/r)
+    TopPad.SetBottomMargin(0.04/r)
+    TopPad.SetRightMargin(0.05)
+    TopPad.SetTicks(1, 1)
+pass
+
+def setBotPad(BotPad, r=4):
+    BotPad.SetPad("BotPad", "", 0., 0., 1.0, 1./r, 0, -1, 0)
+    BotPad.SetTopMargin(r/100.)
+    BotPad.SetBottomMargin(r/10.)
+    BotPad.SetRightMargin(0.05)
+    BotPad.SetTicks(1, 1)
+pass
+
+def addOverflow(hist, addUnder=True):
+    n = hist.GetNbinsX()
+    hist.SetBinContent(n, hist.GetBinContent(n) + hist.GetBinContent(n+1))
+    hist.SetBinError(n, math.sqrt( hist.GetBinError(n)**2 + hist.GetBinError(n+1)**2 ) )
+    hist.SetBinContent(n+1, 0.)
+    hist.SetBinError(n+1, 0.)
+    if addUnder:
+        hist.SetBinContent(1, hist.GetBinContent(0) + hist.GetBinContent(1))
+        hist.SetBinError(1, math.sqrt( hist.GetBinError(0)**2 + hist.GetBinError(1)**2 ) )
+        hist.SetBinContent(0, 0.)
+        hist.SetBinError(0, 0.)
+pass
