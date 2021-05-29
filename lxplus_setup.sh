4be7d0235e621fcf1748314e9affd79b68402efe
SiewYan <siew.yan.hoh@cern.ch>
2021-05-29 07:56:10 +0200

    standardized environment

--- lxplus_setup.sh
+++ lxplus_setup.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-echo "source /cvmfs/sft.cern.ch/lcg/views/LCG_95apython3/x86_64-centos7-gcc7-opt/setup.sh"
-source /cvmfs/sft.cern.ch/lcg/views/LCG_95apython3/x86_64-centos7-gcc7-opt/setup.sh
+# set your environment
+# ROOT version
+# LCG (LHC Computing Grid); python; gcc version
+
+#source /cvmfs/sft.cern.ch/lcg/views/LCG_95apython3/x86_64-centos7-gcc7-opt/setup.sh
+
+source /cvmfs/sft.cern.ch/lcg/views/setupViews.sh LCG_98python3 x86_64-centos7-gcc8-opt
+
+# https://root.cern/releases/release-62400/#installations-in-cvmfs
+#source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.24.00/x86_64-centos7-gcc48-opt/bin/thisroot.sh
